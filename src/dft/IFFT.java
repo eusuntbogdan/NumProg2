a10df5110ccf7d70f143bb13bf6c4dc868bfb5e7
marenress <117991393+marenress@users.noreply.github.com>
2022-12-12 00:06:26 +0100

    Update IFFT.java

--- src/dft/IFFT.java
+++ src/dft/IFFT.java
@@ -15,7 +15,28 @@ public class IFFT {
      * Zweierpotenz ist. Es gilt also: c.length == 2^m fuer ein beliebiges m.
      */
     public static Complex[] ifft(Complex[] c) {
-        // TODO: diese Methode ist zu implementieren
-        return null;
+        Complex[] invers = new Complex[c.length];
+        if(c.length == 1){
+            invers[0] = c[0];
+        }
+        else{
+            int m = c.length/2;
+            // make two arrays one with all numbers ind even places of c and one with all numbers in uneven places of c
+            Complex[] evenPart = new Complex[m];
+            Complex[] unevenPart = new Complex[m];
+            for(int i = 0; i < m; i++){
+                evenPart[i] = c[i*2];
+                unevenPart[i] = c[i*2+1];
+            }
+            evenPart = ifft(evenPart);
+            unevenPart = ifft(unevenPart);
+            Complex omega = new Complex(Math.cos(2*Math.PI*1/c.length),Math.sin(2*Math.PI*1/c.length));
+            for(int j = 0; j < m; j++ ){
+                Complex secondPart = unevenPart[j].mul(omega);
+                invers[j] = evenPart[j].add(secondPart);
+                invers[m+j] = evenPart[j].sub(secondPart);
+            }
+        }
+        return invers;
     }
 }
